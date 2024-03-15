f190df6e5ceaac06e33f81ddda230453655ba8c0
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-03-15 23:54:37 +0700

    Create App.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,24 @@
+{
+  "zen": "Approachable is better than simple.",
+  "hook_id": 466762392,
+  "hook": {
+    "type": "App",
+    "id": 466762392,
+    "name": "web",
+    "active": true,
+    "events": [
+      "installation_target",
+      "meta"
+    ],
+    "config": {
+      "content_type": "json",
+      "insecure_ssl": "0",
+      "secret": "********",
+      "url": "https://rathanaphon.github.io/firebas.origin/"
+    },
+    "updated_at": "2024-03-15T16:41:24Z",
+    "created_at": "2024-03-15T16:41:24Z",
+    "app_id": 856987,
+    "deliveries_url": "https://api.github.com/app/hook/deliveries"
+  }
+}
